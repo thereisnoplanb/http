c12a2fc2fb6d8995f1418e9c7be51ce77dc14001
golangframework <server.ozga@gmail.com>
2025-07-13 08:20:55 +0200

    Add Flush() method to implement http.Flusher interface.

--- statusRecorder/customResponseWriter.go
+++ statusRecorder/customResponseWriter.go
@@ -7,11 +7,17 @@ type responseWriter struct {
 	statusCode int
 }
 
-func (responseWriter *responseWriter) WriteHeader(statusCode int) {
-	responseWriter.statusCode = statusCode
-	responseWriter.ResponseWriter.WriteHeader(statusCode)
+func (response *responseWriter) WriteHeader(statusCode int) {
+	response.statusCode = statusCode
+	response.ResponseWriter.WriteHeader(statusCode)
 }
 
-func (responseWriter *responseWriter) StatusCode() int {
-	return responseWriter.statusCode
+func (response *responseWriter) StatusCode() int {
+	return response.statusCode
+}
+
+func (response *responseWriter) Flush() {
+	if flusher, ok := response.ResponseWriter.(http.Flusher); ok {
+		flusher.Flush()
+	}
 }
